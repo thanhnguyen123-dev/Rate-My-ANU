aca4410634d0efc625a403910a994bf9b66063ac
Thanh Nguyen <lethanh300504@gmail.com>
2025-05-06 15:12:07 +1000

    remove horizontal scroll bar

--- src/components/dashboard/courses-grid.tsx
+++ src/components/dashboard/courses-grid.tsx
@@ -52,7 +52,7 @@ const CoursesGrid = () => {
 
   return (
     <div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 overflow-x-hidden">
         {allCourses.map((course) => (
           <CourseCard 
             key={course?.courseCode}
